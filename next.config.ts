2138f50a3f3ffed6a95c5debd112cfc8656d48bc
koridal <hjpark002@gmail.com>
2025-08-31 20:07:36 +0900

    fix node-module & next.config

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,16 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
-        port: '',
-      }
-    ]
-  }
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
+  },
+  experimental: {
+    turbopackScopeHoisting: false, // 🔑 Turbopack 최적화 비활성화
+  },
+  // Sanity 라이브러리 강제로 트랜스파일
+  transpilePackages: ["sanity", "next-sanity", "@sanity/client"],
 };
 
 export default nextConfig;
